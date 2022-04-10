41447d78a31a80b474c06ddbec8d65c98691e619
michaelzhouy <15602409303@163.com>
2022-04-10 11:21:36 +0800

    batch_size

--- ruddit/roberta_large4/run.sh
+++ ruddit/roberta_large4/run.sh
@@ -1,5 +1,5 @@
 cd warmup
-python -m torch.distributed.launch --nproc_per_node=8 train.py > train.txt
+python -m torch.distributed.launch --nproc_per_node=3 train.py > train.txt
 cd ..
 cd train
 python -m torch.distributed.launch --nproc_per_node=8 train.py > train.txt
