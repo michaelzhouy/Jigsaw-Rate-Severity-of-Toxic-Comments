90430b27b53ccb149b1558a4b78378032b785c27
michaelzhouy <15602409303@163.com>
2022-04-11 09:50:12 +0800

    nproc_per_node=8

--- ruddit/deberta_large4/run.sh
+++ ruddit/deberta_large4/run.sh
@@ -1,8 +1,8 @@
 cd warmup
-python -m torch.distributed.launch --nproc_per_node=3 train.py > train.txt
+python -m torch.distributed.launch --nproc_per_node=8 train.py > train.txt
 cd ..
 cd train
-python -m torch.distributed.launch --nproc_per_node=3 train.py > train.txt
+python -m torch.distributed.launch --nproc_per_node=8 train.py > train.txt
 cd ..
 cd valid
 python valid.py > valid.txt
